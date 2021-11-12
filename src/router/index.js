d16e34c706a03b44c8a668e92cec754d3e07df50
ian038 <ianphua3@gmail.com>
2021-11-12 16:41:44 -0500

    Added navigation

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,31 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Login from '../views/Login'
+import Register from '../views/Register'
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+    meta: {
+      title: "Login",
+      auth: false,
+    },
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+    meta: {
+      title: "Register",
+      auth: false,
+    },
   },
 ];
 
